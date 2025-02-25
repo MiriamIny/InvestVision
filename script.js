b60cac1ddebf4e8dea013fe2aa924da1141b438b
MiriamIny <miny@wits.edu>
2025-02-24 23:31:11 -0500

    fixed html submit, js result, and updates css

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
-function getCompoundInterest(initialAmount, rate, retirementYr)
+function getCompoundInterest()
 { 
+    // Get form values
+    const initialAmount = parseFloat(document.getElementById('initialInvt').value); 
+    const rate = parseFloat(document.getElementById('interestRate').value); 
+    const retirementYr = parseInt(document.getElementById('retirementYr').value);
+
+    console.log(initialAmount)
+    console.log(rate)
+    console.log(retirementYr)
     // Calculate the number of years until retirement
     const currentYear = parseInt(new Date().getFullYear());
     const time = retirementYr-currentYear;
@@ -9,19 +17,10 @@ function getCompoundInterest(initialAmount, rate, retirementYr)
     const futureValue = initialAmount * Math.pow(1+(rate/100), time);
     
     // Display result
-    const resultDiv = document.getElementById('result');
-    resultDiv.textContent = `Your investment will grow to $${futureValue.toFixed(2)} by the year ${retirementYr}.`;
-    resultDiv.hidden = false;
+    const result = document.getElementById('result-message');
+    result.textContent = `Your investment will grow to $${futureValue.toFixed(2)} by the year ${retirementYr}.`;
+    result.hidden = false;
 
     // Stops the form from submitting and refreshing 
     return false; 
 }
-document.getElementById('calculateBtn').addEventListener('click', function() {
-
-    // Get form values
-    const initialAmount = parseFloat(document.getElementById('initialInvt').value); 
-    const rate = parseFloat(document.getElementById('interestRate').value); 
-    const retirementYr = parseInt(document.getElementById('retirementYr').value); 
-
-    getCompoundInterest(initialAmount, rate, retirementYr);
-  });
